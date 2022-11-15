9cd3d2cc7290e9e2b79d5286a2982d6ea159b6ea
Tom Trompeter <63081891+ttrompeter@users.noreply.github.com>
2022-11-15 10:02:06 -0800

    - Revert naming to Modal View

--- GitLynchTutorial/ModalView.swift
+++ GitLynchTutorial/ModalView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ModalView: View {
     var body: some View {
         VStack {
-            Text("This is the Second View")
+            Text("This is the Modal View")
         }
-        .navigationBarTitle("Second View", displayMode: .inline)
+        .navigationBarTitle("Modal View", displayMode: .inline)
     }
 }
 
